35493cd2521ac1a1175e718cc1c8826e569970ea
CJH\bbizi <bbizimtot@naver.com>
2024-02-21 15:59:28 +0900

    add log Test

--- src/main/java/com/bbizimtot/quickstart/QuickstartApplication.java
+++ src/main/java/com/bbizimtot/quickstart/QuickstartApplication.java
@@ -3,10 +3,14 @@ package com.bbizimtot.quickstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class QuickstartApplication {
 
 	public static void main(String[] args) {
+		log.info("Spring Test");
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
 
