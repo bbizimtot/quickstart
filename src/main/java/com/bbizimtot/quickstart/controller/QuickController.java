01e7b78458dded29f740a6372c902c4255f9ab1a
CJH\bbizi <bbizimtot@naver.com>
2024-02-21 14:54:18 +0900

    First

---
+++ src/main/java/com/bbizimtot/quickstart/controller/QuickController.java
@@ -0,0 +1,22 @@
+package com.bbizimtot.quickstart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@RestController
+@Slf4j
+public class QuickController {
+    
+    @GetMapping("/dummy")
+    public String dummy() {
+        return "{}";
+    }
+    
+    @GetMapping("/dummy2")
+    public String dummy2() {
+        return "dummy2";
+    }
+}
