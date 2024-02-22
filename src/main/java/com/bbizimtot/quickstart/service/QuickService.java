51bb7607a5203771fad185e73765acd851602b4e
CJH\bbizi <bbizimtot@naver.com>
2024-02-22 13:13:05 +0900

    convert mybatis to jpa

--- src/main/java/com/bbizimtot/quickstart/service/QuickService.java
+++ src/main/java/com/bbizimtot/quickstart/service/QuickService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bbizimtot.quickstart.dto.ItemDto;
+import com.bbizimtot.quickstart.entity.ItemEntity;
 import com.bbizimtot.quickstart.mapper.QuickMapper;
+import com.bbizimtot.quickstart.repository.ItemRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,30 +19,58 @@ public class QuickService {
     @Autowired
     private QuickMapper quickMapper;
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     public Boolean registerItem(ItemDto itemDto) {
 
-        HashMap<String, String> paramMap = new HashMap<>();
-        paramMap.put("id", itemDto.getId());
-        paramMap.put("name", itemDto.getName());
+        //-------mybatis-------
+        // HashMap<String, String> paramMap = new HashMap<>();
+        // paramMap.put("id", itemDto.getId());
+        // paramMap.put("name", itemDto.getName());
 
-        quickMapper.registerItem(paramMap);
+        // quickMapper.registerItem(paramMap);
 
-        log.info("Insert ...");
+        // log.info("Insert ...");
+
+        // return true;
+        //-------mybatis-------
+
+        //-------jpa-------
+
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setId(itemDto.getId());
+        itemEntity.setName(itemDto.getName());
+
+        itemRepository.save(itemEntity);
 
         return true;
+        //-------jpa-------
     }
 
     public ItemDto getItemById(String id) {
 
-        HashMap<String, String> paramMap = new HashMap<>();
-        paramMap.put("id", id);
+        //-------mybatis-------
+        // HashMap<String, String> paramMap = new HashMap<>();
+        // paramMap.put("id", id);
 
-        HashMap<String, String> res = quickMapper.findById(paramMap);
+        // HashMap<String, String> res = quickMapper.findById(paramMap);
 
+        // ItemDto itemDto = new ItemDto();
+        // itemDto.setId(res.get("ID"));
+        // itemDto.setName(res.get("NAME"));
+
+        // return itemDto;
+        //-------mybatis-------
+
+        //-------jpa-------
+        ItemEntity itemEntity = itemRepository.findById(id).get();
         ItemDto itemDto = new ItemDto();
-        itemDto.setId(res.get("ID"));
-        itemDto.setName(res.get("NAME"));
+
+        itemDto.setId(itemEntity.getId());
+        itemDto.setName(itemEntity.getName());
 
         return itemDto;
+        //-------jpa-------
     }
 }
